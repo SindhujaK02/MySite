7d8f6efbe18c7cf1d0de3105053cbd318c03c002
Sindhuja K <ksindhuja02@gmail.com>
2024-02-10 19:09:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+function redirectToProject1() {
+    window.location.href = 'Drums/index.html'; // Replace with the actual path to Project2
+}
+
+function redirectToProject2() {
+    window.location.href = 'Dice/dicee.html'; // Replace with the actual path to Project3
+}
